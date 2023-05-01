73d80fb56b44fe4d714f334e464387960d6a92ed
Chase Morgan <chasemorgan15@yahoo.com>
2023-05-01 06:22:32 -0400

    added second feature

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,25 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
+    },
+
+
+    getFortune: (req, res) => {
+        const fortunes = ["A fresh start will put you on your way.", "A lifetime friend shall soon be made.", "A pleasant surprise is waiting for you.","All the effort you are making will ultimately pay off.","Believe in yourself and others will too."];
+      
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortune);
+    },
+
+    saveQuote: (req,res) =>{
+        let quotes = []
+        
+        const {quote} = req.body
+        quotes.push(quote)
+
+        res.status(200).send(`Quote "${quote}" has been saved successfully.`)
     }
 
 }
--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
+//imports the express and cor library 
 const express = require("express");
 const cors = require("cors");
 
+//creates an instance of the express application
 const app = express();
 
+//tells the application to use cor middleware - handles core express
 app.use(cors());
 
+//allows the application to parse JSON
 app.use(express.json());
 
+//imports the get compliment functions 
 const { getCompliment } = require('./controller')
+const { getFortune } = require('./controller')
+const { saveQuote } = require('./controller')
 
+//get route for the endpoints which will call the getCompliment function
 app.get("/api/compliment", getCompliment);
+app.get("/api/fortune", getFortune);
+app.post("/api/quote",saveQuote)
 
+//starts the server and tells it to listen to server 4000
 app.listen(4000, () => console.log("Server running on 4000"));
